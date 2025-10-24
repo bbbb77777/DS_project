dce067910384e9446b3a469018aade18c14b1997
bbbb77777 <ben89092419@gmail.com>
2025-10-24 14:22:03 +0800

    tutorial

---
+++ Python/git.py
@@ -0,0 +1,23 @@
+# Mac：
+# /Users/yungm/Documents/DS_test
+# 上传：
+# 1.查看文件状态：
+# git status
+# 2.添加文件到 Git 暂存区：
+# 一个文件：
+# git add new_script.py
+# 多个文件：
+# git add .
+# 3.提交更改：
+# git commit -m "添加 new_script.py 文件"
+# “”为必填
+# 4.查看提交状态：
+# git status
+# 成功的话会显示
+# On branch main
+# nothing to commit, working tree clean
+
+# 5.推送本地代码到 GitHub：
+# git push origin main
+# origin 是远程仓库的默认名称。
+# main 是你当前的分支名称（如果你的分支名是 master，替换为 master）。
